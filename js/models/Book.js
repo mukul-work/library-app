a77e06b98883b6c6093c6a578f6c2aa9fd0e2299
Mukul <mukulkashyap.business@gmail.com>
2026-02-28 23:12:14 +0530

    Add data container

--- js/models/Book.js
+++ js/models/Book.js
@@ -1,1 +1,11 @@
+export default function Book(title, author, pages, read){
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
+Book.prototype.toggleRead = function(){
+    this.read = !this.read;
+}
